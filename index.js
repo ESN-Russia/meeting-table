439e363512a0cc9ae7c08ef5ee9a51dd678d5e7f
Andrey Sokolov <sokolov-as-i@yandex.ru>
2017-07-08 01:13:40 +0300

    start commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express'),
+	app = express(),
+	middleware = require('./middleware')(app, express),
+    http = require('http').Server(app),
+	io = require('socket.io')(http);
+	//socket = require('./middleware/socket')(io);
+
+http.listen(app.get('port'), function() {
+	console.log("Server has started at port " + app.get('port'));
+});
