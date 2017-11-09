f2a754dfd16e01dd0c2a4ff80df44e271e804bba
Andrey Sokolov <sokolov-as-i@yandex.ru>
2017-11-09 19:50:29 +0300

    add sync between master

--- public/master.js
+++ public/master.js
@@ -15,14 +15,6 @@ var AddQuestion = function() {
     $("#q_list").append("<li contenteditable=true>Asker</li>");
 };
 
-var UpdateQuestionList = function() {
-    var q_list = [];
-    $("#q_list").children().toArray().forEach(function (e, i) {
-        q_list = q_list.concat(e.innerHTML);
-    })
-    socket.emit("q_list update", q_list);
-};
-
 var UpdatePresenter = function() {
     socket.emit("set_presenter", {p_name: $("#p_name").val(), p_title: $("#p_title").val()});
 }
@@ -36,4 +28,23 @@ $(document).ready(function() {
 
 socket.on('sync_active_displays', function(msg) {
     $('#display-count').text(msg);
+});
+
+socket.on("set_timer", function(msg) {
+    console.log(msg);
+    $("#t_hour").val(msg.t_hour);
+    $("#t_min").val(msg.t_min);
+    $("#t_event").val(msg.event_name);
+});
+
+socket.on("update_mode", function(msg) {
+    $(".mode_label").removeClass('active');
+    $("#" + msg).prop('checked', true);
+    $("#" + msg).parent().addClass("active");
+});
+
+socket.on("set_presenter", function(msg) {
+    console.log(msg);
+    $("#p_name").val(msg.p_name);
+    $("#p_title").val(msg.p_title);
 });
